55b6248b59328fa4550c8592d392939486431a29
dishant1123 <dishantshah1417@gmail.com>
2025-09-16 19:43:48 +0530

    ...

---
+++ 16sept_intro.py
@@ -0,0 +1,121 @@
+# print("hello dishant")
+"""print('hello dishant shah')
+
+print("smeet") 
+print("live in UK")
+"""
+# comment : 
+"""
+2  type  : 
+
+1.  single line comment  : # 
+2. multi line  comment  :  """ """  or  ''' ''' 
+"""
+
+# python  ==> comma  create  space  in python   
+
+"""
+print("my name  smeet","live in UK.")
+"""
+
+# escape sequence character : 
+
+"""
+1. \n :  new line  
+2. \t :  tab 
+3. \b : backspace 
+4. end= : remove  white  space at the  end  of  sentence. 
+"""
+# smeet gajjar 
+"""
+print("smeet",end=" ")
+print("gajjar")
+"""
+
+# variable declration : 
+"""
+1. not  start with special  character, digit , end ==> special  character . 
+2. exception  : _ 
+
+"""
+"""
+a =10   # static value  ==> fix 
+print(a)
+"""
+
+# data type  : 
+"""
+1. int  : pos  or  negative   only value  store  
+2. float :  decimal value 
+3. string  :  text , name  , special  character , digits 
+4. boolean :  true or false
+5 .complex number  : 2 part  :  1. real  2. imaginary
+"""
+
+"""a=10
+print(a)
+print(type(a))  # type  casting 
+
+b=1234567812345678923456789234567892345678
+print(b)
+print("b=",b)
+print(type(b))  # type  casting 
+
+c=12.67 
+print(c)
+print(type(c))  # type  casting
+
+d=122344.1234567834564567
+print(d)
+print(type(d))  # type  casting
+
+e ="smeet"
+print(e)
+print(type(e))  # type  casting
+
+f= True 
+print(f)
+print(type(f))
+
+g = 90 +7j  # real  part : 90  imaginary  part : 7j 
+print(g)
+print(type(g))
+h=100 + 90j 
+
+print(g+h)
+"""
+
+# user  input string   : 
+
+"""
+a=str(input("enter the  your name  :"))
+b=input("enter the  your sur-name  :")
+print(a,end=" ")
+print(b)
+"""
+# print(a+" "+b)  # concatenate both string  
+
+# user input  number  : 
+
+"""a= int(input("enter the  number  1: "))
+b= int(input("enter the  number  2: "))
+print(a)
+print(b)
+print(a+b)
+"""
+
+"""a= float(input("enter the  number  1: "))
+b= float(input("enter the  number  2: "))
+print(a)
+print(b)
+print(a+b)
+"""
+
+# task  :1 
+"""
+ask user  to enter the  two integer type  number  and  concate them. 
+input  a=10 
+input b=20 
+output  : 1020 
+
+"""
