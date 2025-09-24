b2dec2209e56a5cd1b39696029baea1a7ed0b5e8
dishant1123 <dishantshah1417@gmail.com>
2025-09-24 20:07:46 +0530

    ...

--- 24sept.py
+++ 24sept.py
@@ -64,3 +64,103 @@ print(t1.index(100,1,20))
 input  : t1=  (1,2,3,4,5,6,7,8,9,10)
 output : t1 =(1,2,3,4,5,6,7,8,9,10,"smeet")
 """
+
+# mcq : 
+"""
+t1 = (1,2,3,4,[6,7,8] ,90)
+t1[4].append("smeet")
+print(t1)
+
+a.1,2,3,4,[6,7,8,"smeet"] ,90
+b.1,2,3,4,[6,7,8],"smeet",90
+c.1,2,3,4,"smeet",[6,7,8],90
+d. error beacuse tuple is immutable.
+"""
+
+"""t1 = (1,2,3,4,[6,7,8],90)
+#     0 1 2 3 4
+t1[4].append("smeet")
+print(t1)
+"""
+
+# dict : mutable  sequence of key-value pairs.
+
+"""
+d1={"phy":90,"che":78} 
+# phy ==> key value  90   che ==> key 78 
+
+print(d1)
+print(type(d1))
+
+d2={34 :78 ,99 :"maths"}
+print(d2)
+"""
+
+# update  or add : 
+
+"""
+d1={"phy":90,"che":78} 
+
+d1["maths"]=93  # add 
+d1["phy"]=89  # update 
+print(d1)
+"""
+
+# built  in function :  len  min  max  sorted  sum
+
+"""
+d1={"phy":90,"che":78} 
+
+print(len(d1))
+print(min(d1))
+print(max(d1))
+print(sorted(d1))
+"""
+
+# no slicing  in dict : 
+
+"""
+d1={"phy":90,"che":78} 
+print(d1[0])  # error : bcz of  no slicing  poss in dict.
+"""
+
+# method : 
+d1={"phy":90,"che":78} 
+
+"""d1.clear()
+print(d1)
+"""
+
+"""d2=d1.copy()
+print(d2)
+"""
+
+"""print(d1.keys())
+print(d1.values())
+print(d1.items())
+"""
+d1={"phy":90,"che":78,"maths":99} 
+
+# print(d1.get("phy")) # arg : key 
+
+"""l1= ["dishant","smeet"]
+# {"dishant":100,"smeet":100}
+
+d2 =dict.fromkeys(l1,100)
+d2["dishant"]=101
+print(d2)
+
+d1.update(d2)
+print(d1)
+"""
+"""d1.pop("phy")
+print(d1)
+"""
+"""d1.popitem()
+print(d1)
+"""
+"""d1.setdefault("ss",100)
+print(d1)
+"""
+
+# list  , tuple  ,dict:
