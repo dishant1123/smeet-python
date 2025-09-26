ce266b23e88b22cdf7b2bf879137bef486e84d1a
dishant1123 <dishantshah1417@gmail.com>
2025-09-26 17:14:21 +0530

    ....

---
+++ 26sept.py
@@ -0,0 +1,54 @@
+# string  : immutable sequence of characters ==> can't be changed
+
+"""
+s1= "my name is smeet."
+print(s1)
+print(type(s1))
+"""
+
+# built in function  :  len min max  sorted sum
+
+"""
+s1= "my name is smeet."
+
+print(len(s1))
+print(min(s1))
+print(max(s1))
+print(sorted(s1))
+"""
+# slicing : 
+"""s1= "my name is smeet."
+
+print(s1[2])
+print(s1[3])
+print(s1[-3])
+print(s1[2 : 5])
+print(s1[2 :  ])
+print(s1[  : 6])
+
+print(s1[1 : 6 :2])
+print(s1[  :  :2])
+print(s1[  :  :-2])
+print(s1[  :  :-1])
+"""
+
+"""
+task :1 
+input  a = dishant dipakkumar shah 
+output :  d.d.shah
+
+task :2 
+
+ask user to enter the  two  string  and  swap the  first letter  of  the  second  string  with  the  first  letter  of  the  first  string.
+
+input  a: color 
+input  b: full 
+
+ouptut a: fulor 
+output b: coll
+"""
+
+    
+
+
+
